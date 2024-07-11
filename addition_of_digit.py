957f08280a37aa5fea1b21a8db43262a206e9818
sanikavavhal07 <sanikavavhal007@gmail.com>
2024-07-11 16:52:54 +0530

    Add files via upload

---
+++ addition_of_digit.py
@@ -0,0 +1,8 @@
+two_digit=input("Enter a Two digit number: ")
+first_digit = two_digit[0]
+second_digit = two_digit[1]
+result = int(first_digit) + int(second_digit)
+print("Addition of digit is : ",result)
+
+
+
