66778a3894ef9b6af7aeb5300c161bc46105fe29
sanikavavhal07 <sanikavavhal007@gmail.com>
2024-07-14 16:41:44 +0530

    Update age_in_weeks.py

--- age_in_weeks.py
+++ age_in_weeks.py
@@ -6,10 +6,10 @@ days = round(age * 365)
 print(f"You have completed {month} months, {week} weeks and {days} days")
 
 
-# age = input("What is your current age? ")
-# years = 90 - int(age)
-# months = round(years * 12)
-# weeks = round(years * 52)
-# days = round(years * 365)
+age = input("What is your current age? ")
+years = 90 - int(age)
+months = round(years * 12)
+weeks = round(years * 52)
+days = round(years * 365)
 
-# print(f"You have {days} days, {weeks} weeks, and {months} months left.")
+print(f"You have {days} days, {weeks} weeks, and {months} months left.")
